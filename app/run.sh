6515d7eca07098a0ed6a730b3b6ef8c6e6feec7a
Joeri van Dooren <ure@moreorless.be>
2016-06-23 10:07:46 +0200

    cleanup

--- app/run.sh
+++ app/run.sh
@@ -1,6 +1,13 @@
-#!/bin/bash -x
+#!/bin/bash
 
-echo "oc:x:`id -u`:0:oc:/wssh:/sbin/nologin" >> /etc/passwd
+# create working user
+echo "oc:x:`id -u`:0:oc:/wssh:/bin/bash" >> /etc/passwd
 
+# run ssh on 2222
 /usr/sbin/sshd -p 2222
+
+# diplay private key
+cat /wssh/.ssh/key
+
+# start websocket proxy
 wsshd
